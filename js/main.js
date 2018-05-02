a5ce35ba0a52d96b13915e39df0ec61c08698fe5
Patrick Klostermann <dev@netflair.at>
2018-05-02 23:16:53 +0200

    added function to highlight current profile card name

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Get Radio buttons
+var radio = document.getElementsByTagName('input');
+
+//Get Lables
+var sabine = document.getElementById('sabLab');
+var patrick = document.getElementById('patLab');
+var maryam = document.getElementById('marLab');
+var mario = document.getElementById('mioLab');
+
+//Get Radio inputs
+var sabCheck = document.getElementById('sabCheck');
+var patCheck = document.getElementById('patCheck');
+var marCheck = document.getElementById('marCheck');
+var mioCheck = document.getElementById('mioCheck');
+
+var btnContainer = document.getElementById("nameSelect");
+var btns = btnContainer.getElementsByClassName("state");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
+
